a196e96dfd3bfc4190d04462ed2e1c9d4319b1c0
Paulo H Albanez <phalbanez@gmail.com>
2025-07-28 10:58:16 -0300

    Aula 13 e 14 - Implementando paginação e ordenação

--- Source/Backend/src/providers/Providers.CRUD.pas
+++ Source/Backend/src/providers/Providers.CRUD.pas
@@ -3,13 +3,11 @@ unit Providers.CRUD;
 interface
 
 uses
-  System.SysUtils, System.Classes, System.Generics.Collections, System.JSON,
-  Providers.Connection, FireDAC.Stan.Intf,
-  FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf,
-  FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys,
-  FireDAC.Phys.FB, FireDAC.Phys.FBDef, FireDAC.ConsoleUI.Wait,
-  FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt, Data.DB,
-  FireDAC.Comp.DataSet, FireDAC.Comp.Client, FireDAC.VCLUI.Wait;
+  System.SysUtils, System.Classes, System.Generics.Collections, System.JSON, Providers.Connection,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf,
+  FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.FB,
+  FireDAC.Phys.FBDef, FireDAC.ConsoleUI.Wait, FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf,
+  FireDAC.DApt, Data.DB, FireDAC.Comp.DataSet, FireDAC.Comp.Client, FireDAC.VCLUI.Wait;
 
 type
   TProviderCrud = class(TProviderConnection)
@@ -18,7 +16,8 @@ type
     qryRecordCount: TFDQuery;
     qryRecordCountCOUNT: TLargeintField;
   private
-    { Private declarations }
+    procedure SetOrdenation(const AQuery: TFDQuery; const AQueryParam: TDictionary<string, string>);
+    procedure SetPagination(const AQuery: TFDQuery; const AQueryParam: TDictionary<string, string>);
   public
     function Append(const AValue: TJSONObject): Boolean; virtual;
     function Update(const AValue: TJSONObject): Boolean; virtual;
@@ -65,8 +64,67 @@ end;
 
 function TProviderCrud.ListAll(const AQueryParams: TDictionary<string, string>): TDataSet;
 begin
+  SetPagination(qryPesquisa, AQueryParams);
+  SetOrdenation(qryPesquisa, AQueryParams);
   qryPesquisa.Open();
   Result := qryPesquisa;
+end;
+
+procedure TProviderCrud.SetOrdenation(const AQuery: TFDQuery;
+  const AQueryParam: TDictionary<string, string>);
+var
+  LSort: TArray<string>;
+  LValues: TArray<string>;
+  LOrdenation: string;
+  LFieldName: string;
+  LOrderAscDesc: string;
+begin
+  if (not AQueryParam.ContainsKey('sort')) then
+    Exit;
+
+  LSort := AQueryParam.Items['sort'].Split([';']);
+
+  LOrdenation := EmptyStr;
+  for var LSortItem in LSort do
+  begin
+    LValues := LSortItem.Split([',']);
+    if Length(LValues) = 0 then
+      Continue;
+
+    LFieldName := LValues[0].Trim;
+    if AQuery.Fields.FindField(LFieldName) = nil then
+      Continue;
+
+    if not LOrdenation.IsEmpty then
+      LOrdenation := LOrdenation + ', ';
+
+    LOrdenation := LOrdenation + LFieldName;
+
+    if Length(LValues) >= 2 then
+    begin
+      LOrderAscDesc := LValues[1].ToLower;
+      if LOrderAscDesc.Equals('asc') or LOrderAscDesc.Equals('desc') then
+        LOrdenation := LOrdenation + ' ' + LOrderAscDesc;
+    end;
+  end;
+
+  if not LOrdenation.Trim.IsEmpty then
+    AQuery.SQL.Add('order by ' + LOrdenation);
+end;
+
+procedure TProviderCrud.SetPagination(const AQuery: TFDQuery;
+  const AQueryParam: TDictionary<string, string>);
+begin
+  if AQueryParam.ContainsKey('limit') then
+  begin
+    AQuery.FetchOptions.RecsMax := StrToIntDef(AQueryParam.Items['limit'], 50);
+    AQuery.FetchOptions.RowsetSize := AQuery.FetchOptions.RecsMax;
+  end;
+
+  if AQueryParam.ContainsKey('offset') then
+  begin
+    AQuery.FetchOptions.RecsSkip := StrToIntDef(AQueryParam.Items['offset'], 0);
+  end;
 end;
 
 function TProviderCrud.Update(const AValue: TJSONObject): Boolean;
