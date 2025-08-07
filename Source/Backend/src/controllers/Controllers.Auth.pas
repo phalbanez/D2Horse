b41229c842fa79281778ce2eb013d0a725ed0114
Paulo H Albanez <phalbanez@gmail.com>
2025-08-07 14:52:15 -0300

    Aula 20 - Implementando o refresh token

--- Source/Backend/src/controllers/Controllers.Auth.pas
+++ Source/Backend/src/controllers/Controllers.Auth.pas
@@ -7,7 +7,7 @@ procedure Register;
 implementation
 
 uses
-  System.JSON, Horse, Services.Users;
+  System.SysUtils, System.JSON, Horse, Services.Users;
 
 procedure Login(Req: THorseRequest; Res: THorseResponse);
 var
@@ -16,7 +16,7 @@ var
   LToken: TJSONObject;
 begin
   if (not Req.Body<TJSONObject>.TryGetValue<string>('email', LEmail)) or
-     (not Req.Body<TJSONObject>.TryGetValue<string>('password', LPassword)) then
+    (not Req.Body<TJSONObject>.TryGetValue<string>('password', LPassword)) then
     raise EHorseException.New.Status(THTTPStatus.BadRequest).Error('Informe usuário e senha');
 
   LService := TServiceUser.Create;
@@ -24,26 +24,55 @@ begin
     if (not LService.IsValidUser(LEmail, LPassword)) then
       raise EHorseException.New.Status(THTTPStatus.Unauthorized).Error('Usuário ou senha inválido');
 
-      LToken := TJSONObject.Create;
-      try
-        LToken.AddPair('accessToken', LService.GetAccessToken);
-        LToken.AddPair('refreshToken', LService.GetRefreshToken);
-      except
-        LToken.Free;
-        raise;
-      end;
+    LToken := TJSONObject.Create;
+    try
+      LToken.AddPair('accessToken', LService.GetAccessToken);
+      LToken.AddPair('refreshToken', LService.GetRefreshToken);
+    except
+      LToken.Free;
+      raise;
+    end;
 
-      Res.Send<TJSONObject>(LToken);
+    Res.Send<TJSONObject>(LToken);
 
   finally
     LService.Free;
   end;
 end;
 
+procedure RefreshToken(Req: THorseRequest; Res: THorseResponse);
+var
+  LSubject: string;
+  LService: TServiceUser;
+  LToken: TJSONObject;
+begin
+  LSubject := Req.Session<TJSONObject>.GetValue<string>('sub');
+
+  LService := TServiceUser.Create;
+  try
+    if (LService.GetById(LSubject.ToInteger).IsEmpty) or (not LService.qryCadastroACTIVE.AsBoolean)
+    then
+      raise EHorseException.New.Status(THTTPStatus.Unauthorized).Error('Refresh token inválido');
+
+    LToken := TJSONObject.Create;
+    try
+      LToken.AddPair('accessToken', LService.GetAccessToken);
+      LToken.AddPair('refreshToken', LService.GetRefreshToken);
+    except
+      LToken.Free;
+      raise;
+    end;
+
+    Res.Send<TJSONObject>(LToken);
+  finally
+    LService.Free;
+  end;
+end;
 
 procedure Register;
 begin
-  THorse.Post('/login', Login)
+  THorse.Post('/login', Login);
+  THorse.Get('/refresh', RefreshToken);
 end;
 
 end.
